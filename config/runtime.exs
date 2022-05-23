2d96bfde0a00b204d9d958271d95d6099f964922
Dustin Rouillard <code@dstn.to>
2022-05-23 12:21:36 -0600

    Add REDIS_DSN as env

--- config/runtime.exs
+++ config/runtime.exs
@@ -8,5 +8,6 @@ if config_env() == :prod do
     bot_presence: System.get_env("BOT_PRESENCE") || "you <3",
     bot_presence_type: String.to_integer(System.get_env("BOT_PRESENCE_TYPE") || "3"),
     bot_token: System.get_env("BOT_TOKEN"),
-    redis_uri: System.get_env("REDIS_URI") || System.get_env("REDIS_URL")
+    redis_uri:
+      System.get_env("REDIS_DSN") || System.get_env("REDIS_URI") || System.get_env("REDIS_URL")
 end
