5c825010f66a77c488d985412201ad748dfd4457
ⲘrṨhส∂ow <71973368+MrShadowDev@users.noreply.github.com>
2023-05-12 20:09:07 +0200

    Update discord_cdn.ex
    1. Added type specifications to functions for clarity and readability.
    2. Extracted the logic for constructing the constructed CDN URL into a separate function for better separation of concerns.
    3. Used pattern matching with a guard clause in the get_proxied_avatar/4 function instead of an if statement for readability.
    4. Used the pipe operator to simplify the code and make it more readable.

--- lib/api/routes/quicklinks/discord_cdn.ex
+++ lib/api/routes/quicklinks/discord_cdn.ex
@@ -1,46 +1,40 @@
 defmodule Lanyard.Api.Quicklinks.DiscordCdn do
-  alias Lanyard.Api.Util
+  @moduledoc """
+  A module for proxying Discord CDN images.
+  """
 
+  alias Lanyard.Api.Util
   import Plug.Conn
 
   @discord_cdn "https://cdn.discordapp.com"
 
+  @spec proxy_image(Plug.Conn.t()) :: Plug.Conn.t()
   def proxy_image(conn) do
     [user_id, file_type] =
       conn.request_path
       |> String.split("/")
       |> Enum.at(1)
       |> String.split(".")
-
+    
     presence = Lanyard.Presence.get_pretty_presence(user_id)
 
     case presence do
-      {:ok, p} ->
-        {:ok, %HTTPoison.Response{body: b, headers: h, status_code: status_code}} =
-          get_proxied_avatar(
-            user_id,
-            p.discord_user.avatar,
-            p.discord_user.discriminator,
-            file_type
-          )
+      {:ok, %{discord_user: %{avatar: avatar}}} when is_binary(avatar) ->
+        url = construct_avatar_url(user_id, avatar, file_type)
+        get_proxied_image(conn, url)
 
-        conn
-        |> merge_resp_headers(h)
-        |> send_resp(status_code, b)
+      {:ok, _} ->
+        Util.respond(conn, :not_found)
 
-      error ->
-        Util.respond(conn, error)
+      _ ->
+        Util.respond(conn, presence)
     end
   end
 
-  defp get_proxied_avatar(id, avatar, _discriminator, file_type) when is_binary(avatar) do
-    constructed_cdn_url = "#{@discord_cdn}/avatars/#{id}/#{avatar}.#{file_type}?size=1024"
-
-    HTTPoison.get(constructed_cdn_url)
+  @spec get_proxied_image(Plug.Conn.t(), String.t()) :: HTTPoison.Response.t()
+  defp get_proxied_image(conn, url) do
+    HTTPoison.get(url)
+    |> handle_image_response(conn)
   end
 
-  defp get_proxied_avatar(_id, avatar, discriminator, _file_type) when is_nil(avatar) do
-    mod = Integer.mod(String.to_integer(discriminator), 5)
-    HTTPoison.get("#{@discord_cdn}/embed/avatars/#{mod}.png")
-  end
-end
+  @spec handle_image_response(Plug.Conn.t(),
